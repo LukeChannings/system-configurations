5cb0a098a86cab685af15580c9644be2d2292d0f
Luke Channings <461449+LukeChannings@users.noreply.github.com>
2024-07-04 22:31:21 +0100

    atuin - remote sync and some other settings

--- modules/common/shell/atuin.nix
+++ modules/common/shell/atuin.nix
@@ -1,26 +1,28 @@
-{ config, lib, ... }: {
+{ config, lib, ... }:
+{
   options.shell.atuin.enable = lib.mkEnableOption "atuin";
 
   # https://github.com/nix-community/home-manager/blob/master/modules/programs/atuin.nix
   config = lib.mkIf config.shell.atuin.enable {
     home-manager.users.${config.user}.programs.atuin = {
       enable = true;
-      flags = [ "--disable-up-arrow" ];
       # https://docs.atuin.sh/configuration/config/
       settings = {
-        auto_sync = false;
-        workspaced = true;
+        auto_sync = true;
+        workspaces = true;
         ctrl_n_shortcuts = true;
         dialect = "uk";
         filter_mode = "host";
+        filter_mode_shell_up_key_binding = "directory";
         style = "compact";
         inline_height = 5;
         show_help = false;
+        enter_accept = true;
 
         sync = {
           records = true;
         };
-    };
       };
     };
+  };
 }
