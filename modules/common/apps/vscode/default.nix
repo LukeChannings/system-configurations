e1561073982277c4c23485794750d428070b8ef5
Luke Channings <461449+LukeChannings@users.noreply.github.com>
2024-07-16 19:12:13 +0100

    Add adrs tool, update vscode settings

--- modules/common/apps/vscode/default.nix
+++ modules/common/apps/vscode/default.nix
@@ -34,38 +34,7 @@
           enableExtensionUpdateCheck = false;
           mutableExtensionsDir = false;
 
-          userSettings = {
-            "editor.fontFamily" = "RecMonoLinear Nerd Font";
-            "editor.fontSize" = 16;
-            "notebook.markup.fontSize" = 14;
-            "terminal.integrated.fontSize" = 14;
-            "editor.fontLigatures" = true;
-            "explorer.confirmDelete" = false;
-            "editor.rulers" = [ 120 ];
-            "editor.multiCursorModifier" = "ctrlCmd";
-            "workbench.colorTheme" = "One Dark Pro Flat";
-            "editor.suggestSelection" = "first";
-            "typescript.updateImportsOnFileMove.enabled" = "always";
-            "editor.renderWhitespace" = "all";
-            "emmet.showSuggestionsAsSnippets" = true;
-            "editor.inlineSuggest.enabled" = true;
-            "redhat.telemetry.enabled" = false;
-            "editor.snippetSuggestions" = "bottom";
-            "extensions.ignoreRecommendations" = true;
-            "cSpell.language" = "en;en-GB";
-            "workbench.startupEditor" = "none";
-            "window.commandCenter" = false;
-            "terminal.integrated.shellIntegration.enabled" = false;
-            "workbench.editor.empty.hint" = "hidden";
-            "search.quickOpen.history.filterSortOrder" = "recency";
-            "gitlens.showWelcomeOnInstall" = false;
-            "gitlens.showWhatsNewAfterUpgrades" = false;
-            "gitlens.plusFeatures.enabled" = false;
-            "git.openRepositoryInParentFolders" = "never";
-            "[markdown]" = {
-              "editor.wordWrap" = "on";
-            };
-          };
+          userSettings = lib.importJSON ./settings.json;
 
           extensions = with pkgs.vscode-extensions; [
             zhuangtongfa.material-theme
